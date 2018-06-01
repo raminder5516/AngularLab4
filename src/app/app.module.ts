b2d8643e7be7dfd3c09320c013925e40b5061b1f
raminder <raminderkaurmaan@yahoo.com>
2018-06-01 13:09:41 -0400

    part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Lab4Component } from './lab4/lab4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lab4Component
   ],
   imports: [
     BrowserModule
